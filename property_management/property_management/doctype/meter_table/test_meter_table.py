f04af31f521d03f79d0c565bdc3eb47d1f21e3a1
MW <m.widmann@widmanndesign.com>
2023-05-26 15:14:21 +0200

    Draft

---
+++ property_management/property_management/doctype/meter_table/test_meter_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Axessio.de / Marius Widmann and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMeterTable(FrappeTestCase):
+	pass
---
+++ property_management/property_management/doctype/meter_type/meter_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Axessio.de / Marius Widmann and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class MeterType(Document):
+	pass
